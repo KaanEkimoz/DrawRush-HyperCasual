df54fdb821bcf220560527acfbd60bad3c003a41
Yusuf Kaan Usta <205040053@stu.istinye.edu.tr>
2022-05-18 15:13:19 +0300

    Changed Added
    Siuuu

--- Assets/DontDestroyOnLoad.cs
+++ Assets/DontDestroyOnLoad.cs
@@ -25,7 +25,6 @@ public class DontDestroyOnLoad : MonoBehaviour
                     Destroy(gameObject);
                 }
             }
-            
         }
         DontDestroyOnLoad(gameObject);
     }
