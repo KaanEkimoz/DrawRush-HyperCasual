f0194d75254b33dede1fde4cf36cf92241f63b76
Yusuf Kaan Usta <205040053@stu.istinye.edu.tr>
2022-05-18 15:06:30 +0300

    DontDestroyObjects Added
    Levels Added

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    [HideInInspector] public string objectID;
+    private DontDestroyOnLoad[] dontDestroyObjects;
+    private void Awake()
+    {
+        objectID = name + transform.position.ToString() + transform.eulerAngles.ToString();
+        dontDestroyObjects = FindObjectsOfType<DontDestroyOnLoad>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < dontDestroyObjects.Length; i++)
+        {
+            if (dontDestroyObjects[i] != this)
+            {
+                if (dontDestroyObjects[i].objectID == objectID)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
